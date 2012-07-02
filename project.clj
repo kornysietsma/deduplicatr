4021845a5a37e102676fffd6a2f29db41fe2e93e
Korny Sietsma <korny@sietsma.com>
2012-07-03 00:02:44 +1000

    added basic main method

--- project.clj
+++ project.clj
@@ -5,9 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :dependencies {org.clojure/clojure "1.4.0",
-                 commons-codec/commons-codec "1.4"}
+                 commons-codec/commons-codec "1.4"
+                 org.clojure/tools.cli "0.2.1"}
   :profiles {:dev {:dependencies {midje "1.4.0",
                                   lein-midje "2.0.0-SNAPSHOT"
                                   com.stuartsierra/lazytest "1.2.3"
                                  }}}
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.0.0"
+  :main deduplicatr.core
+  :jvm-opts ["-Xss1500m"] 
+)
--- src/deduplicatr/core.clj
+++ src/deduplicatr/core.clj
@@ -1,6 +1,39 @@
-(ns deduplicatr.core)
+(ns deduplicatr.core
+  (:use [clojure.tools.cli :only [cli]]
+        [clojure.java.io :only [file]]
+        [deduplicatr.duplicates :only [duplicates]]
+        [deduplicatr.fstree :only [treeify]])
+  (:import (deduplicatr.file FileSummary DirSummary))
+  (:gen-class :main true))
+
+(defn show-duplicates
+  [root]
+  (let [tree (treeify root)
+        _ (println "tree parsed")
+        dups (duplicates tree)
+        _ (println "dups found")]
+  (doseq [files dups]
+    (println (count files) " files of size " (.bytes (first files)))
+    (doseq [summary files]
+      (if (instance? DirSummary summary)
+        (println "   " (str (.getPath (.file summary)) "/ (" (.filecount summary) " files)"))
+        (println "   " (.getPath (.file summary)))
+        )))))
 
 (defn -main
-  "I don't do a whole lot."
+  "The main entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [[options args banner]
+        (cli args
+             ["-h" "--help" "Show help" :default false :flag true]
+             ["-root" "root directory to scan" :default "." :parse-fn #(file %)]
+             )
+        root (file (:root options))]
+    (when (:help options)
+      (println banner)
+      (System/exit 0))
+    (when (not (.isDirectory root))
+      (println root " is not a valid directory")
+      (System/exit 0))
+    (show-duplicates root)
+    ))
