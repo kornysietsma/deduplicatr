8e24b6b4c7eaefda6d281e655118708183850514
Korny Sietsma <korny@sietsma.com>
2013-01-24 14:01:24 +1100

    updated uberdocs, updated project with latest deps, new TODOs

--- project.clj
+++ project.clj
@@ -3,14 +3,13 @@
   :url "https://github.com/kornysietsma/deduplicatr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
-  :dependencies {org.clojure/clojure "1.4.0",
-                 commons-codec/commons-codec "1.4"
-                 org.clojure/tools.cli "0.2.1"}
-  :profiles {:dev {:dependencies {midje "1.4.0",
-                                  lein-midje "2.0.0-SNAPSHOT"
-                                  com.stuartsierra/lazytest "1.2.3"
-                                 }}}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [commons-codec/commons-codec "1.4"]
+                 [bultitude "0.1.7"]      ; <<<<==== fix for https://github.com/marick/lein-midje/issues/35
+                 [org.clojure/tools.cli "0.2.2"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]
+                   :plugins [[lein-midje "2.0.4"]
+                             [lein-marginalia "0.7.1"]]  }}
   :min-lein-version "2.0.0"
   :main deduplicatr.core
   :jvm-opts ["-Xmx1024m" "-Xss100m"] 
